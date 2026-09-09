5ef7e7bca8fb3887b76b14be4c51d738d40d0993
Riwaj Bastola <riwajbastola2001@gmail.com>
2026-09-08 23:51:37 -0400

    week3

---
+++ week3.py
@@ -0,0 +1,37 @@
+# Write a Python function to flatten a nested list.
+# Input: [[1, 2], [3, 4], [5]]
+# Output: [1, 2, 3, 4, 5]
+
+
+def flatten():
+    x = [[1, 2], [3, 4], [5]]
+    y = []
+
+    for i in x:
+        for j in i:
+            y.append(j)
+    return y
+
+print(flatten())
+
+# Merge Two Sorted Lists
+# Write a Python function to merge two sorted lists into a single sorted list.
+# Input: [1, 3, 5], [2, 4, 6]
+# Output: [1, 2, 3, 4, 5, 6]
+
+def merge():
+    a = [1, 3, 5]
+    b = [2, 4, 6]
+    c = []
+    i=0
+    j=0
+    while i<len(a) and j<len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i=i+1
+        else:
+            c.append(b[j])
+            j=j+1
+    print(c)
+
+merge()
